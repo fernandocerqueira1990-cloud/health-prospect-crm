9d12d692558fc3c7f08a2afa15968f6b88ac6536
Fernando Cerqueira <fernando.cerqueira1990@gmail.com>
2026-08-21 14:46:51 -0300

    feat(sprint-11): schedule commercial alert generation

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('commercial:notifications')
+    ->hourly()
+    ->withoutOverlapping(30)
+    ->onOneServer()
+    ->runInBackground()
+    ->appendOutputTo(storage_path('logs/commercial-scheduler.log'));
